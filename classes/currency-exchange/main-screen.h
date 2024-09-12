53b08633214313d26f9a13f15c5a711f1037d42a
Ahmed Mohamed <amohamedj29@gmail.com>
2024-09-12 12:44:55 +0300

    edit files name

---
+++ classes/currency-exchange/main-screen.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "../main/screen.h"
+#include <iostream>
+
+// Screens
+#include "./convert-currencies-screen.h"
+#include "./edit-currency-screen.h"
+#include "./find-currency-screen.h"
+#include "./list-currencies-screen.h"
